9a92e8cdacfa4103e06807020b99f182a1853cc5
regeshengen <reges@unochapeco.edu.br>
2020-07-28 18:34:32 -0300

    criando middleware e integrando com index

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const app = express()
+const db = require('./config/db')
+const consign = require('consign')
+
+consign().then('./config/middlewares.js').into(app)
+
+app.db = db
 
 app.get('/', (req, res) => {
     res.status(200).send('Backend executando...')
